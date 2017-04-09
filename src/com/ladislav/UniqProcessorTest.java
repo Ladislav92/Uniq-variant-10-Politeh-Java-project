2ae0836373eb24ecbca80b62682bbcebc4a429bf
Ladislav92 <milunovic.ladislav92@gmail.com>
2017-04-09 23:21:55 +0300

    Testing in progress, still have some unsolved problems

--- src/com/ladislav/UniqProcessorTest.java
+++ src/com/ladislav/UniqProcessorTest.java
@@ -1,13 +1,12 @@
 package com.ladislav;
 
 import junit.framework.TestCase;
-import org.junit.Test;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by Ladislav on 4/6/2017.
@@ -15,13 +14,24 @@ import java.util.List;
 
 public class UniqProcessorTest extends TestCase {
 
-    @Test
+    public void testCloseIONullPointerException() throws Exception {
+        UniqProcessor processorClient = UniqProcessor.getInstance();
+
+        try {
+            processorClient.closeIO();
+            fail("Should throw NullPointerException if processor isn't initialized");
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+        }
+    }
+
     public void testProcessAndOutput() throws Exception {
 
         /*
          * FIRST test case: from file to console
          * All flags set except -o fileName flag
          */
+        UniqProcessor processor = UniqProcessor.getInstance();
 
         final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
@@ -29,7 +39,6 @@ public class UniqProcessorTest extends TestCase {
         String[] arguments = {"-i", "-u", "-s", "2", "-c", "input_file.txt"};
         String[] expectedResults = {"1 : FirstOne", "1 : aaaa", "1 : aaaa", "1 : oooo", "1 : LastOne"};
 
-        ProcessorClient processor = new ProcessorClient(UniqProcessor.getInstance());
         processor.init(arguments);
         processor.processAndOutput();
 
@@ -46,7 +55,6 @@ public class UniqProcessorTest extends TestCase {
         arguments = new String[]{"-i", "-u", "-s", "2", "-c", "-o", "output_file.txt", "input_file.txt"};
         expectedResults = new String[]{"1 : FirstOne", "1 : aaaa", "1 : aaaa", "1 : oooo", "1 : LastOne"};
 
-        processor = new ProcessorClient(UniqProcessor.getInstance());
         processor.init(arguments);
         processor.processAndOutput();
         processor.closeIO();
@@ -77,7 +85,6 @@ public class UniqProcessorTest extends TestCase {
         ByteArrayInputStream inputStream = new ByteArrayInputStream(testInput.getBytes());
         System.setIn(inputStream);
 
-        processor = new ProcessorClient(UniqProcessor.getInstance());
         processor.init(arguments);
         processor.processAndOutput();
         processor.closeIO();
@@ -108,7 +115,6 @@ public class UniqProcessorTest extends TestCase {
         expectedResults = new String[]{"Quick", "Brown", "Fox", "Jumps", "Over", "The", "Lazy", "Dog"};
 
 
-        processor = new ProcessorClient(UniqProcessor.getInstance());
         processor.init(arguments);
         processor.processAndOutput();
         processor.closeIO();
@@ -137,7 +143,6 @@ public class UniqProcessorTest extends TestCase {
         expectedResults = new String[]{"16 : Quick"};
 
 
-        processor = new ProcessorClient(UniqProcessor.getInstance());
         processor.init(arguments);
         processor.processAndOutput();
         processor.closeIO();
@@ -167,7 +172,6 @@ public class UniqProcessorTest extends TestCase {
                 "JUMPS", "Over", "OVER", "The", "THE", "Lazy", "LAZY", "Dog", "DOG"};
 
 
-        processor = new ProcessorClient(UniqProcessor.getInstance());
         processor.init(arguments);
         processor.processAndOutput();
         processor.closeIO();
@@ -196,7 +200,6 @@ public class UniqProcessorTest extends TestCase {
         expectedResults = new String[]{};
 
 
-        processor = new ProcessorClient(UniqProcessor.getInstance());
         processor.init(arguments);
         processor.processAndOutput();
         processor.closeIO();
@@ -212,51 +215,252 @@ public class UniqProcessorTest extends TestCase {
 
     }
 
-    public void testOutput1() throws Exception {
-
-
-    }
-
     public void testProcessAndTestOutput() throws Exception {
 
 
+        /*
+         * FIRST test case: from file to console
+         * All flags set except -o fileName flag
+         */
+        UniqProcessor processor = UniqProcessor.getInstance();
+
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        String[] arguments = {"-i", "-u", "-s", "2", "-c", "input_file.txt"};
+        String[] expectedResults = {"1 : FirstOne", "1 : aaaa", "1 : aaaa", "1 : oooo", "1 : LastOne"};
+
+        processor.init(arguments);
+        processor.process();
+        processor.output();
+
+        String[] realOut = outputStream.toString().split("\n");
+        for (int i = 0; i < expectedResults.length; i++) {
+            assertEquals(expectedResults[i], realOut[i].trim());
+        }
+        outputStream.reset();
+
+        /*
+         * SECOND test case: from file to file
+         * All flags set
+         */
+        arguments = new String[]{"-i", "-u", "-s", "2", "-c", "-o", "output_file.txt", "input_file.txt"};
+        expectedResults = new String[]{"1 : FirstOne", "1 : aaaa", "1 : aaaa", "1 : oooo", "1 : LastOne"};
+
+        processor.init(arguments);
+        processor.process();
+        processor.output();
+        processor.closeIO();
+
+        Path p = Paths.get("output_file.txt");
+        File testFile = p.toFile();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(testFile))) {
+
+            for (String anExpectedResult : expectedResults) {
+                assertEquals(anExpectedResult, br.readLine().trim());
+            }
+        } finally {
+            Files.delete(p);
+        }
+
+        /*
+         *  THIRD test case: from console to file
+         *  Flags set: -c, -s 2, -i
+         */
+        String testInput = "FirstOne\naaaa\nbbbb\nbbbb\naaaa\ncccc\nCCcc\nabFF\nccFF\noooo\nzzzz\nzzzz\n" +
+                "test\ntest\ntest\nTest\nTesT\ncd\nab\nLastOne\n";
+
+        arguments = new String[]{"-i", "-s", "2", "-c"};
+        expectedResults = new String[]{"1 : FirstOne", "1 : aaaa", "2 : bbbb", "1 : aaaa",
+                "2 : cccc", "2 : abFF", "1 : oooo", "2 : zzzz", "5 : test", "2 : cd", "1 : LastOne"};
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(inputStream);
+
+        processor.init(arguments);
+        processor.process();
+        processor.output();
+        processor.closeIO();
+
+        realOut = outputStream.toString().split("\n");
+
+        for (int i = 0; i < expectedResults.length; i++) {
+            assertEquals(expectedResults[i], realOut[i].trim());
+        }
+
+        inputStream.reset();
+        outputStream.reset();
+        /*
+         *  FOURTH test case: from console to console
+         *  Note: I/O tested. Need to test few different cases of args and input
+         *  With different input and combination
+         *  For simplicity, testing will go from console to console
+         *  Args: -i only
+         */
+        testInput = "Quick\nQUICK\nBrown\nBROWN\nFox\nFOX\nJumps\nJUMPS\nOver\nOVER\nThe\nTHE\nLazy\nLAZY\nDog\nDOG\n";
+
+        inputStream = new ByteArrayInputStream(testInput.getBytes());
+
+        System.setOut(new PrintStream(outputStream));
+        System.setIn(inputStream);
+
+        arguments = new String[]{"-i"};
+        expectedResults = new String[]{"Quick", "Brown", "Fox", "Jumps", "Over", "The", "Lazy", "Dog"};
+
+
+        processor.init(arguments);
+        processor.process();
+        processor.output();
+        processor.closeIO();
+
+        realOut = outputStream.toString().split("\n");
+
+        for (int i = 0; i < expectedResults.length; i++) {
+            assertEquals(expectedResults[i], realOut[i].trim());
+        }
+
+        inputStream.reset();
+        outputStream.reset();
+
+        /*
+         * FIFTH test case: -c -s 7
+         * Checking when -s flag is greater than every word size
+         */
+        testInput = "Quick\nQUICK\nBrown\nBROWN\nFox\nFOX\nJumps\nJUMPS\nOver\nOVER\nThe\nTHE\nLazy\nLAZY\nDog\nDOG\n";
+
+        inputStream = new ByteArrayInputStream(testInput.getBytes());
+
+        System.setOut(new PrintStream(outputStream));
+        System.setIn(inputStream);
+
+        arguments = new String[]{"-c", "-s", "7"};
+        expectedResults = new String[]{"16 : Quick"};
+
+
+        processor.init(arguments);
+        processor.process();
+        processor.output();
+        processor.closeIO();
+
+        realOut = outputStream.toString().split("\n");
+
+        for (int i = 0; i < expectedResults.length; i++) {
+            assertEquals(expectedResults[i], realOut[i].trim());
+        }
+
+        inputStream.reset();
+        outputStream.reset();
+
+        /*
+         *  SIXTH test case: no args
+         */
+
+        testInput = "Quick\nQUICK\nBrown\nBROWN\nFox\nFOX\nJumps\nJUMPS\nOver\nOVER\nThe\nTHE\nLazy\nLAZY\nDog\nDOG\n";
+
+        inputStream = new ByteArrayInputStream(testInput.getBytes());
+
+        System.setOut(new PrintStream(outputStream));
+        System.setIn(inputStream);
+
+        arguments = new String[]{};
+        expectedResults = new String[]{"Quick", "QUICK", "Brown", "BROWN", "Fox", "FOX", "Jumps",
+                "JUMPS", "Over", "OVER", "The", "THE", "Lazy", "LAZY", "Dog", "DOG"};
+
+
+        processor.init(arguments);
+        processor.process();
+        processor.output();
+        processor.closeIO();
+
+        realOut = outputStream.toString().split("\n");
+
+        for (int i = 0; i < expectedResults.length; i++) {
+            assertEquals(expectedResults[i], realOut[i].trim());
+        }
+
+        inputStream.reset();
+        outputStream.reset();
+
+        /*
+         * SEVENTH case: empty input
+         */
+
+        testInput = "";
+
+        inputStream = new ByteArrayInputStream(testInput.getBytes());
+
+        System.setOut(new PrintStream(outputStream));
+        System.setIn(inputStream);
+
+        arguments = new String[]{};
+        expectedResults = new String[]{};
+
+        processor.init(arguments);
+        processor.process();
+        processor.output();
+        processor.closeIO();
+
+        realOut = outputStream.toString().split("\n");
+
+        for (int i = 0; i < expectedResults.length; i++) {
+            assertEquals(expectedResults[i], realOut[i].trim());
+        }
+
+        inputStream.reset();
+        outputStream.reset();
+
     }
 
-}
+    public void testOutputWithParams() throws Exception {
 
-/**
- * Dependency Injection, has purpose to simplify testing
- * Takes UniqProcessor as parameter in constructor and delegates its methods
- */
-class ProcessorClient {
+        /**
+         * Testing client with no args, just output needed - output is already tested
+         * Since no args output will be to console
+         */
 
-    UniqProcessor up;
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
 
-    public ProcessorClient(UniqProcessor up) {
-        this.up = up;
+        String testInput[] = {"FirstOne", "aaaa", "bbbb", "LastOne"};
+
+        ArrayList<EntryPair<String, Integer>> testList = new ArrayList<>();
+        for (String s : testInput) {
+            testList.add(new EntryPair<>(s, 1));
+        }
+
+        String[] args = {};
+
+        UniqProcessor processor = UniqProcessor.getInstance();
+
+        processor.init(args);
+        processor.output(testList);
+        processor.closeIO();
+
+        String[] realOut = outputStream.toString().split("\n");
+
+        for (int i = 0; i < testInput.length; i++) {
+            assertEquals(testInput[i], realOut[i]);
+        }
+
     }
 
-    public void init(String[] arguments) {
-        up.init(arguments);
+    public void testCloseIOIllegalStateException(){
+        String input = "Hey Joe Where You Goin' With That Gun Of Yours";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        String[] args = {};
+
+        UniqProcessor processor = UniqProcessor.getInstance();
+
+        processor.init(args);
+        processor.closeIO();
+
+        try {
+            processor.process();
+            fail("Should throw IllegalStateException if I/O is closed");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalStateException);
+        }
     }
 
-    public List<EntryPair<String, Integer>> process() {
-        return up.process();
-    }
-
-    public void processAndOutput() {
-        up.processAndOutput();
-    }
-
-    public void output() {
-        up.output();
-    }
-
-    public void output(List<EntryPair<String, Integer>> list) {
-        up.output(list);
-    }
-
-    public void closeIO() {
-        up.closeIO();
-    }
 }
